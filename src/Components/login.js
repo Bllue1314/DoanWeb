910a0c5db2530e5aaf1a06cd602eba81ab3ab519
Blue <nguyenblue1310@gmail.com>
2025-10-17 23:37:04 +0700

    Update popup

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,24 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btnLogin = document.getElementById("btn-login");
-    const loginOverlay = document.querySelector(".login");
-    const baseContainer = document.querySelector(".base-container");
-    const nav = document.querySelector("nav");
+const login = document.querySelector('.login');
+const btnLogin = document.getElementById('btn-login');
 
-    btnLogin.addEventListener("click", () => {
-        loginOverlay.style.display = "flex";
-        baseContainer.style.filter = "blur(6px)";
-        nav.style.filter = "blur(6px)";
-        baseContainer.style.pointerEvents = "none";
-        nav.style.pointerEvents = "none";
-    });
+btnLogin.addEventListener('click', () => {
+    login.classList.add('active');
+});
 
-    loginOverlay.addEventListener("click", (e) => {
-        if (e.target === loginOverlay) {
-        loginOverlay.style.display = "none";
-        baseContainer.style.filter = "none";
-        nav.style.filter = "none";
-        baseContainer.style.pointerEvents = "auto";
-        nav.style.pointerEvents = "auto";
-        }
-    });
+login.addEventListener('click', (e) => {
+    if (e.target === login) {
+        login.classList.remove('active');
+    }
 });
